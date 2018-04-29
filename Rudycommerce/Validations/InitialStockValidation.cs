307f73593e8273b22359fb9e33dc6488ea9bbc84
laure <laure@DESKTOP-PKK44LS>
2018-04-29 19:27:26 +0200

    Trying validations

---
+++ Rudycommerce/Validations/InitialStockValidation.cs
@@ -0,0 +1,30 @@
+﻿using RudycommerceLibrary.Utilities.Validations;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Rudycommerce.Validations
+{
+    public class InitialStockValidation : ValidationRule
+    {
+        // https://docs.microsoft.com/en-us/dotnet/framework/wpf/data/how-to-implement-binding-validation
+
+        public override ValidationResult Validate(object input, CultureInfo cultureInfo)
+        {
+            string errorMessage = ProductValidations.InitialStockValidation(input);
+
+            if (errorMessage == null)
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, errorMessage);
+            }
+        }
+    }
+}
--- RudycommerceLibrary/Utilities/Validations/ProductValidations.cs
+++ RudycommerceLibrary/Utilities/Validations/ProductValidations.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace RudycommerceLibrary.Utilities.Validations
 {
-    public class ProductValidations
+    public static class ProductValidations
     {
+        public static string InitialStockValidation(object input)
+        {
+            try
+            {
+                int initialStock = int.Parse((string)input);
 
+                if (initialStock < 0)
+                {
+                    return "NO-ML Please enter a positive number";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return "NO-ML Please enter a valid number";
+            }            
+        }
+        public static string UnitPriceValidation(object input)
+        {
+            return null;
+        }
     }
 }
